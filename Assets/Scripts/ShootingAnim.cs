9bdf458327b522289886b6ba494ef959eca8e4b6
jericko <jericko.ordonez@gmail.com>
2018-11-03 06:29:29 -0700

    small edits to animations

---
+++ Assets/Scripts/ShootingAnim.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingAnim : MonoBehaviour
+{
+    public Animator anim;
+
+    private KeyCode Space;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(Space))
+            anim.Play("shoot");
+    }
+}
