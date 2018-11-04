8a251163283c930aeb4b589f208d5762722bc32c
Unknown <MOTYSHIZ@gmail.com>
2018-11-04 10:18:26 -0800

    Script and prefab edits for proper object-oriented practice.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : BoardEntity
+public class Player : EntityWithStatus
 {
     [SerializeField] GameObject[] attacks;
 
